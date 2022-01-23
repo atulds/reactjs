d6ec13ca8d500d589eff751cc61de63a5eea763b
Atul <atul.parashar@dotsquares.com>
2022-01-23 18:03:53 +0530

    chnage text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload docker jenkins tyestoimg
+          Edit <code>src/App.js</code> and save to reload docker jenkins tse
         </p>
         <a
           className="App-link"
