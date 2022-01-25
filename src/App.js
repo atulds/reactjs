3fe51ac785c271dcf37a01e17ff43113689e162a
Atul <atul.parashar@dotsquares.com>
2022-01-25 12:40:48 +0530

    docker swar react

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload docker jenkins tse
+          Edit <code>src/App.js</code> and save to reload docker jenkins swarm
         </p>
         <a
           className="App-link"
