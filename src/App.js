3094479df44c23e4db3b197305a0ddf87535f012
Atul <atul.parashar@dotsquares.com>
2022-01-25 12:50:07 +0530

    new swarm

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload docker jenkins swarm
+          Edit <code>src/App.js</code> and save to reload docker jenkins swarm new
         </p>
         <a
           className="App-link"
