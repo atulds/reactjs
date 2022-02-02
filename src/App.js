be9bea45e479d04b0cf731df2e410ab9889e12a2
Atul <atul.parashar@dotsquares.com>
2022-02-02 14:00:12 +0530

    Add Jenkins File

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload docker jenkins swarm new
+          Edit <code>src/App.js</code> and save to reload docker jenkins newq
         </p>
         <a
           className="App-link"
